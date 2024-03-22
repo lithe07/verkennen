113340b5b2b26a8e13a94ea4871bf38776ea90c7
Lithe Jniad <99078068@mydavinci.nl>
2024-03-22 09:34:50 +0100

    push

--- module-3/deel-2/les-1/wedstrijdteller/wedstrijdteller.js
+++ module-3/deel-2/les-1/wedstrijdteller/wedstrijdteller.js
@@ -1,17 +1,18 @@
 inputTeams = document.getElementById('input-teams');
-
-let game = 
+ 
+let game =
 {
     team1punten : 0,
     team2punten: 0,
     serving : 0,
-    lastServe: 0, // Wie vorige servie
-    lastScored: 0 // wie vorige punt gescord
+    lastService : 0,
+    lastScored : 0,
+    set : 1
 }
-
-inputTeam1.value = '...'; // testdata
-inputTeam2.value = '...'; // testdata
-
+ 
+inputTeam1.value = ''; // testdata
+inputTeam2.value = ''; // testdata
+ 
 function updatescreen(){
     // maak juiste butten geel
     counterTeam2.classList.remove("serving");
@@ -21,16 +22,16 @@ function updatescreen(){
     }else{
         counterTeam2.classList.add("serving");
     }
-
+ 
     counterTeam1.innerText = game.team1punten;
     counterTeam2.innerText = game.team2punten;
-    
+   
 }
-
+ 
 function start(event){
     // controleer if er twee namen zijn ingevoerd
     console.log('You pressed start!');
-    if(nameTeam1.innerText == "..." || nameTeam2.innerText == "..."){
+    if(nameTeam1.innerText == '' || nameTeam2.innerText == ''){
         alert("Er mist een naam!")
         return;
     };
@@ -40,59 +41,76 @@ function start(event){
         alert("Wie moet er beginnen");
         return;
     };
-
+ 
     // Hide inputfiles
     input_teams.style.display = "none";
     counterTeam1.disabled = false;
     counterTeam2.disabled = false;
-
+ 
     // set serving team
     game.serving = 2;
     if (servingTeam1.checked){
         game.serving = 1;
     };
-
+ 
     updatescreen()
-
+ 
 }
+
 startButton.addEventListener('click',start);
 
+function updateSet() {
+    document.getElementById("set-teller").innerHTML = "Set " + game.set;
+}
+
+ 
 function count(event){
-    game.lastServe = game.serving;
+    game.lastService = game.serving
     if (this.id == 'counterTeam1'){
         game.team1punten += 1;
-        game.serving = 1; 
+        game.serving = 1;
         game.lastScored = 1;
     }else{
         game.team2punten += 1;
-        game.serving = 2; 
+        game.serving = 2;
         game.lastScored = 2;
     }
-
     undoButton.disabled = false;
     updatescreen();
+    updateSet();
 }
 counterTeam1.addEventListener('click',count);
 counterTeam2.addEventListener('click',count);
-
+ 
 function displayNames(event){
     nameTeam1.innerText = inputTeam1.value;
     nameTeam2.innerText = inputTeam2.value;
-
+ 
+ 
 }
 inputTeam1.addEventListener('change',displayNames);
 inputTeam2.addEventListener('change',displayNames);
-
-function undoLastpoint(event){
-    // regel hier de rest
+ 
+undoButton.addEventListener('click', undoLastPoint);
+function undoLastPoint(event){
     undoButton.disabled = true;
+    if (game.lastScored == 1) {
+        game.team1punten -= 1;
+        game.serving = 2;
+    } else {
+        game.team2punten -= 1;
+        game.serving = 1;
+    }
+    updatescreen();
 }
-
-undoButton.addEventListener("click",undoLastpoint);
-
+ 
+ 
+ 
+ 
 undoButton.disabled = true;
 counterTeam1.disabled = true;
 counterTeam2.disabled = true;
-
-
-
+ 
+ 
+ 
+ 
